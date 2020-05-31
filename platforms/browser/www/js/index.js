265a79759bc30ef43ed63e7178bfb85334e6e2e9
saul <sarima666@gmail.com>
2020-05-30 22:53:20 -0500

    ultima actualización

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -46,4 +46,56 @@ var app = {
 
         console.log('Received Event: ' + id);
     }
-};
+}
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Para el boton top de las paginas
+//Get the button
+var mybutton1 = document.getElementById("irarriba1");
+var mybutton2 = document.getElementById("irarriba2");
+var mybutton3 = document.getElementById("irarriba3");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton1.style.display = "block";
+    } else {
+    mybutton1.style.display = "none";
+    }if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton2.style.display = "block";
+    } else {
+    mybutton2.style.display = "none";
+    }if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton3.style.display = "block";
+    } else {
+    mybutton3.style.display = "none";
+    } }
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0; }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Para mostrar la fecha
+    var d = new Date(); var month = new Array(12); month[0] = 'Enero';
+    month[1] = 'Febrero'; month[2] = 'Marzo'; month[3] = 'Abril';
+    month[4] = 'Mayo'; month[5] = 'Junio'; month[6] = 'Julio';
+    month[7] = 'Agosto'; month[8] = 'Septiembre'; month[9] = 'Octubre';
+    month[10] = 'Noviembre'; month[11] = 'Diciembre';
+    var todaysDate = + d.getDate() + ' de ' + month[d.getUTCMonth()] + ' del ' + d.getUTCFullYear();
+    document.getElementById('fechahoy').innerHTML = 'Hoy es: ' + todaysDate;
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//Para mosstrar la hora-->
+    function startTime(){
+        today = new Date();
+        h = today.getHours();
+        m = today.getMinutes();
+        s = today.getSeconds();
+        m = checkTime(m);
+        s = checkTime(s);
+        document.getElementById('reloj').innerHTML = h + ":" + m + ":" + s;
+        t = setTimeout('startTime()',500);}
+        function checkTime(i)
+        {if (i < 10) {i = "0" + i; }return i; }
+        window.onload = function(){startTime(); }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////
